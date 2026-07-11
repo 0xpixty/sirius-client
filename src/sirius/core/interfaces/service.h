84c14986f1181afb5a691977f2bf238da3c70b56
xpz12 <b1tpixty@gmail.com>
2026-07-11 23:07:09 +0300

    feat(core): introduce Sirius core interfaces

---
+++ src/sirius/core/interfaces/service.h
@@ -0,0 +1,16 @@
+/* (c) Sirius Client contributors. See licence.txt in the root of the distribution for more information. */
+#ifndef SIRIUS_CORE_INTERFACES_SERVICE_H
+#define SIRIUS_CORE_INTERFACES_SERVICE_H
+
+namespace sirius::core::interfaces
+{
+
+	class IService
+	{
+	public:
+		virtual ~IService() = default;
+	};
+
+} // namespace sirius::core::interfaces
+
+#endif
