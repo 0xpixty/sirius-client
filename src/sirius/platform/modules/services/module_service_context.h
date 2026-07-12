f9680a388b938df446ffd0294ac592ad50b5d4b8
0xpixty <apolemia@icloud.com>
2026-07-12 14:47:46 +0300

    feat(modules): add module service context

---
+++ src/sirius/platform/modules/services/module_service_context.h
@@ -0,0 +1,70 @@
+/* (c) Sirius Client contributors. See licence.txt in the root of the distribution for more information. */
+#ifndef SIRIUS_PLATFORM_MODULES_SERVICES_MODULE_SERVICE_CONTEXT_H
+#define SIRIUS_PLATFORM_MODULES_SERVICES_MODULE_SERVICE_CONTEXT_H
+
+namespace sirius::core::config
+{
+	class CConfigRegistry;
+} // namespace sirius::core::config
+
+namespace sirius::core::events
+{
+	class CEventDispatcher;
+} // namespace sirius::core::events
+
+namespace sirius::core::logging
+{
+	class CLogger;
+} // namespace sirius::core::logging
+
+namespace sirius::core::runtime
+{
+	class CCoreRuntime;
+} // namespace sirius::core::runtime
+
+namespace sirius::core::tasks
+{
+	class CTaskDispatcher;
+} // namespace sirius::core::tasks
+
+namespace sirius::platform::modules
+{
+	class CModuleContext;
+} // namespace sirius::platform::modules
+
+namespace sirius::platform::modules::services
+{
+
+	class CModuleServiceContext final
+	{
+	public:
+		explicit CModuleServiceContext(CModuleContext &Context) noexcept;
+		~CModuleServiceContext() noexcept;
+
+		CModuleServiceContext(const CModuleServiceContext &Other) = delete;
+		CModuleServiceContext &operator=(const CModuleServiceContext &Other) = delete;
+		CModuleServiceContext(CModuleServiceContext &&Other) = delete;
+		CModuleServiceContext &operator=(CModuleServiceContext &&Other) = delete;
+
+		core::runtime::CCoreRuntime &Runtime() noexcept;
+		const core::runtime::CCoreRuntime &Runtime() const noexcept;
+		core::events::CEventDispatcher &Events() noexcept;
+		const core::events::CEventDispatcher &Events() const noexcept;
+		core::config::CConfigRegistry &Config() noexcept;
+		const core::config::CConfigRegistry &Config() const noexcept;
+		core::logging::CLogger &Logger() noexcept;
+		const core::logging::CLogger &Logger() const noexcept;
+		core::tasks::CTaskDispatcher &Tasks() noexcept;
+		const core::tasks::CTaskDispatcher &Tasks() const noexcept;
+
+	private:
+		core::runtime::CCoreRuntime &m_Runtime;
+		core::events::CEventDispatcher &m_Events;
+		core::config::CConfigRegistry &m_Config;
+		core::logging::CLogger &m_Logger;
+		core::tasks::CTaskDispatcher &m_Tasks;
+	};
+
+} // namespace sirius::platform::modules::services
+
+#endif
